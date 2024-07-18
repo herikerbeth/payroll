c19764814ff481cf51813738fe02ce2429cfa1c4
herikerbeth <herik.docarmo@hotmail.com>
2024-07-18 10:18:42 -0300

    Add a new Order record

---
+++ src/main/java/payroll/order/Order.java
@@ -0,0 +1,71 @@
+package payroll.order;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "CUSTOMER_ORDER")
+public class Order {
+
+    private @Id
+    @GeneratedValue Long id;
+
+    private String description;
+    private Status status;
+
+    public Order() {
+    }
+
+    public Order(String description, Status status) {
+        this.description = description;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Order))
+            return false;
+        Order other = (Order) obj;
+        return Objects.equals(this.id, other.id) && Objects.equals(this.description, other.description) && this.status == other.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.description, this.status);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" + "id=" + this.id + ", description='" + this.description + '\'' + ", status=" + this.status + '}';
+    }
+}
