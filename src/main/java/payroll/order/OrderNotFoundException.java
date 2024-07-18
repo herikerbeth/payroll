ebdb44c3ddbce98c56715935f1f9e1212d6e7d7a
herikerbeth <herik.docarmo@hotmail.com>
2024-07-18 10:30:02 -0300

    Create a new exception class

---
+++ src/main/java/payroll/order/OrderNotFoundException.java
@@ -0,0 +1,8 @@
+package payroll.order;
+
+public class OrderNotFoundException extends RuntimeException {
+
+    public OrderNotFoundException(Long id) {
+        super("Could not find order " + id);
+    }
+}
