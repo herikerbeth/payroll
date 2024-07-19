f5e132ded0edbef91f2c8bf03ff026dea477032a
herikerbeth <herik.docarmo@hotmail.com>
2024-07-19 11:57:27 -0300

    Update the database pre-loader

--- src/main/java/payroll/LoadDatabase.java
+++ src/main/java/payroll/LoadDatabase.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 
 import payroll.employee.Employee;
 import payroll.employee.EmployeeRepository;
+import payroll.order.Order;
+import payroll.order.OrderRepository;
 
 @Configuration
 class LoadDatabase {
@@ -15,11 +17,20 @@ class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
     @Bean
-    CommandLineRunner initDatabase(EmployeeRepository repository) {
+    CommandLineRunner initDatabase(EmployeeRepository repository, EmployeeRepository employeeRepository, OrderRepository orderRepository) {
 
         return args -> {
             log.info("Preloading " + repository.save(new Employee("Jose", "Silva", "servente")));
             log.info("Preloading " + repository.save(new Employee("Mario", "Silva", "pedreiro")));
+
+            employeeRepository.findAll().forEach(employee -> log.info("Preloading " + employee));
+
+            orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
+            orderRepository.save(new Order("IPhone", Status.IN_PROGRESS));
+
+            orderRepository.findAll().forEach(order -> {
+                log.info("Preloaded " + order);
+            });
         };
     }
 }
